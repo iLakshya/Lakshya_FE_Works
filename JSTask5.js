ff2f5503c7a36fcfcab2d604c8a53f19dc0626bd
Lakshya Goel <lgoel340@gmail.com>
2022-08-29 17:17:13 +0530

    JS Task 5

--- JSTask5.js
+++ JSTask5.js
@@ -50,3 +50,21 @@ const cricket = [
 // console.log(cricket1);
 
 console.log(cricket.filter(c=>c.retired===true));
+
+
+// Task 5
+/* Write a JavaScript function to get the first element of an array. 
+Passing a parameter 'n' will return the first 'n' elements of the array:
+Test Data :
+console.log(first([7, 9, 0, -2]));
+console.log(first([],3));
+console.log(first([7, 9, 0, -2],3));
+console.log(first([7, 9, 0, -2],6));
+console.log(first([7, 9, 0, -2],-3));
+Expected Output :
+7
+[]
+[7, 9, 0]
+[7, 9, 0, -2]
+[] */
+
