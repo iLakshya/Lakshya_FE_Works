0993fecfb923324a70ce0c505fbc19792491bad0
Lakshya Goel <lgoel340@gmail.com>
2022-08-31 18:11:39 +0530

    JS Task 6

--- JSTask6.js
+++ JSTask6.js
@@ -104,11 +104,29 @@ let sampleData = {
     ]
 };
 
+// Task 1
 /* The match details for semi-final -1 appear to be missing from the data.
 Add new match detail into the matches array without mutating the original Array (set Id number as 4).*/
 
 console.log(sampleData);
 
 // Make new array for the missing team.
-let array1 = {Id: 4, match: 'semi-final', team1: {name: "Germany", code: "GEM"}, team2: {name: "England", code: "ENG"}, knockout: false, stadium: {key: "fisht", name: "Fisht Stadium"}};
+let array1 = [{Id: 4, match: 'semi-final', team1: {name: "Germany", code: "GEM"}, team2: {name: "England", code: "ENG"}, knockout: false, stadium: {key: "fisht", name: "Fisht Stadium"}}];
 console.log(array1);
+// Adding the the missing array to the initial one.
+sampleData.matches.splice(3,0,...array1);
+console.log(sampleData);
+
+
+// Task 2
+// Create a new variable and sort the matches array based on the Id property.
+
+// console.log(sampleData);
+let sort = sampleData.matches.sort((a,b)=>{
+    return a.Id - b.Id;
+});
+console.log(sort); // arranged in ascending order.
+
+
+// Task 3
+//Check whether the matches array contains only knockout matches and save the result to a new variable.
